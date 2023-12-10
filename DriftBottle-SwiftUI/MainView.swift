a28ec3d667f68a46d11e5fa53c57b5410aaa2a19
Shaw Lu <shawlu95@gmail.com>
2023-12-10 00:08:39 -0800

    layout main view

--- DriftBottle-SwiftUI/MainView.swift
+++ DriftBottle-SwiftUI/MainView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .opacity(0.5)
+            
+            VStack {
+                Spacer()
+                
+                Button("Drop a Bottle") {
+                    
+                }
+                .frame(width: 600)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue.opacity(0.7))
+                .padding(.bottom, 20)
+                
+                Button("Pickup a Bottle") {
+                    
+                }
+                .frame(width: 600)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue.opacity(0.7))
+                .padding(.bottom, 20)
+
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
