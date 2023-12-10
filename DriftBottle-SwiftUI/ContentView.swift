94d83d59a81c3cce95d6fcb64cfa465d05bdee78
Shaw Lu <shawlu95@gmail.com>
2023-12-09 23:49:22 -0800

    Initial Commit

---
+++ DriftBottle-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DriftBottle-SwiftUI
+//
+//  Created by main on 2023/12/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DriftBottle-SwiftUI/DriftBottle_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriftBottle_SwiftUIApp.swift
+//  DriftBottle-SwiftUI
+//
+//  Created by main on 2023/12/9.
+//
+
+import SwiftUI
+
+@main
+struct DriftBottle_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
